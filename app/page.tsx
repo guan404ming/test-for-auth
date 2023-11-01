b60dbf05b65aaf5f05d58e188350f2427b2bcfc1
GUAN-MING <guanmingchiu@gmail.com>
2023-11-01 21:35:33 +0800

    feat: add user info

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { signIn, useSession, signOut } from 'next-auth/react';
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from '@/components/ui/avatar';
 
 export default function Home() {
 
@@ -10,10 +15,22 @@ export default function Home() {
     console.log(session, status);
 
     return (
-        <main className='text-center justify-center'>
-            <div className='min-h-screen pt-[44vh]'>
-                <h1 className='font-bold text-4xl mb-10'>Test for Auth</h1>
-                <Button className='mr-5 round-xl' onClick={() => signIn()}>Sign In</Button>
+        <main className='text-center flex items-center justify-center h-screen'>
+            <div id='content'>
+                <h1 className='font-bold text-4xl mb-8'>Test for Auth</h1>
+                {
+                    (session && session.user) ? (
+                        <>
+                            <Avatar className='m-auto'>
+                                <AvatarImage src={`${session.user.image}`} alt="@shadcn" />
+                                <AvatarFallback>{session.user.name}</AvatarFallback>
+                            </Avatar>
+                            <div className='text-xl my-5'>{session.user.name}</div>
+                        </>
+                    ) : <></>
+                }
+
+                <Button className='round-xl mr-5' onClick={() => signIn()}>Sign In</Button>
                 <Button className='round-xl' onClick={() => signOut()}>Sign Out</Button>
             </div>
         </main>
